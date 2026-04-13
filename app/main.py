52cdd9b02ee79611add3cada7657fcdc32f8171f
daiyongqing <daiyongqing@simplylab.xyz>
2026-04-13 19:15:23 +0800

    update

--- app/main.py
+++ app/main.py
@@ -85,4 +85,4 @@ def index():
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("app.main:app", host="127.0.0.1", port=8888, reload=True)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8888, reload=True)
