a7d5793d60899d9390d1fb606335b09a59ccad1e
daiyongqing <daiyongqing@simplylab.xyz>
2026-05-03 17:29:55 +0800

    update

--- scripts/fofa_scraper.py
+++ scripts/fofa_scraper.py
@@ -36,8 +36,8 @@ async def main():
 
     try:
         date = (datetime.now() - timedelta(days=random.randint(1, 730))).strftime("%Y-%m-%d")
-        q_oai = f'(body="gemini") || (body="api" || body="key") after="{date}"'
-        q_ggl = f'(body="AIzaSy{random.choice("ABCD")}") after="{date}"'
+        q_oai = f'((body="gemini") || (body="api" || body="key")) && after="{date}"'
+        q_ggl = f'(body="AIzaSy{random.choice("ABCD")}") && after="{date}"'
         logger.info(f"FOFA 查询[OpenAI]: {q_oai}")
         logger.info(f"FOFA 查询[Google]: {q_ggl}")
 
